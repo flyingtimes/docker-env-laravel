addc70c34be186634b6341f31dd1a404c80eddb8
clark <clark@192.168.0.103>
2020-02-17 15:53:54 +0800

    code

--- quickstart.sh
+++ quickstart.sh
@@ -8,8 +8,7 @@ docker-compose up -d
 mkdir src
 # 调用composer容器，运行laravel初始化命令，将自动通过阿里云镜像源拉取初始化代码
 docker-compose run --rm composer create-project laravel/laravel .
-# 在镜像里，laravel的配置文件中数据库连接配置需要做响应的修改 127.0.0.1->mysql，库名、用户名、密码也要根据docker-compose.yml的配置做相应修改才行
+# 在镜像里，laravel的配置文件中数据库连接配置需要做响应的修改 127.0.0.1->mysql，root的密码也要根据docker-compose.yml的配置做相应修改才行
+# 默认库名为laravel，用户为root
 docker-compose run --rm composer sed -i "s/DB_HOST=127.0.0.1/DB_HOST=mysql/g" /var/www/.env
-docker-compose run --rm composer sed -i "s/DB_DATABASE=/DB_DATABASE=homestead/g" /var/www/.env
-docker-compose run --rm composer sed -i "s/DB_USERNAME=/DB_USERNAME=root/g" /var/www/.env
-docker-compose run --rm composer sed -i "s/DB_PASSWORD=/DB_PASSWORD=secret/g" /var/www/.env
+docker-compose run --rm composer sed -i "s/DB_PASSWORD=/DB_PASSWORD=rootsecret/g" /var/www/.env
